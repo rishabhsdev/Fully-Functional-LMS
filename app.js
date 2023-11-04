4b29c3b4dc2178e2827cc99593f1bd445180e832
Rishabh <56164824+rishabhsdev@users.noreply.github.com>
2023-11-04 09:03:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
+const { DateTime } = require("luxon");
 
 const app = express();
 
@@ -10,24 +11,31 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  const dayOfWeek = today.getDay();
-  const weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentWeekday = weekdays[dayOfWeek].toUpperCase();
+  // const today = new Date();
+  // const dayOfWeek = today.getDay();
+  // const weekdays = [
+  //   "Sunday",
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  // ];
+  // const currentWeekday = weekdays[dayOfWeek].toUpperCase();
+  const timeZone = "Asia/Kolkata";
+
+  // Get the current date in the specified timezone
+  const currentDateTime = DateTime.now().setZone(timeZone);
+
+  // Get the name of the current day of the week
+  const currentWeekday = currentDateTime.toFormat("cccc").toUpperCase();
 
   res.render("index", { currentWeekday });
 });
 
 app.get("/download-book", function (req, res) {
-  const file = "resource/adtemplate1.zip";
+  const file = "resource/Your-Resources.zip";
   res.download(file); // Set disposition and send it.
   const log = `File "${file}" downloaded by ${
     req.ip
@@ -39,5 +47,8 @@ app.get("/download-book", function (req, res) {
   });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.get("/payment-success-3377321", function (req, res) {
+  const currentWeekday = "wed";
+  res.render("payment-success", { currentWeekday });
+});
+app.listen(3000);
