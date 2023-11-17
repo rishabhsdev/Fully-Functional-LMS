706a24838ee573a2800a8f46a334482d74fa1b79
Rishabh <56164824+rishabhsdev@users.noreply.github.com>
2023-11-17 14:24:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.get("/payment-success-3377321", function (req, res) {
   res.render("payment-success");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
